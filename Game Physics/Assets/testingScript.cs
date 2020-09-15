9a9daf00db1e5153fe7de3a67b80a269c0037a89
Ranatza <69979310+Ranatza@users.noreply.github.com>
2020-09-14 21:26:31 -0500

    setup
    created script, plane, and cube to test physics

---
+++ Game Physics/Assets/testingScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+
+    void OnTriggerStay()
+    {
+        Debug.Log("OnTriggerStay");
+    }
+    
+    void OnTriggerExit()
+    {
+        Debug.Log("OnTriggerExit");
+    }
+        
+    void OnCollisionEnter()
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+        
+    void OnCollisionStay()
+    {
+        Debug.Log("OnCollisionStay");
+    }
+        
+    void OnCollisionExit()
+    {
+        Debug.Log("OnCollisionExit");
+    }
+}
